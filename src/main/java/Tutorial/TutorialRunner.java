cb96c5609e4ffd8324a3d6e65bbb0532a071859f
kozefina <hamonnah@gmail.com>
2020-07-16 22:08:58 +0200

    no image

--- src/main/java/Tutorial/TutorialRunner.java
+++ src/main/java/Tutorial/TutorialRunner.java
@@ -2,8 +2,14 @@ package Tutorial;
 
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class TutorialRunner extends Application {
@@ -11,9 +17,23 @@ public class TutorialRunner extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
+            // stage > scene > container > node
+           /*Pane pane = new Pane();
+            Scene scene = new Scene(pane, 800, 800);*/
 
-            Pane pane = new Pane();
-            Scene scene = new Scene(pane, 800, 800);
+            /*Label label1 = new Label("Hello");
+            Label label2 = new Label("World");
+
+            label1.setStyle("-fx-border-color: black; -fx-font-size: 30;");
+            label2.setStyle("-fx-border-color: black; -fx-font-size: 30;");
+
+            gridPane.addRow(0, label1, label2);
+            gridPane.setHgap(10);
+            gridPane.setAlignment(Pos.CENTER);*/
+
+            ImageView whitePiece = new ImageView();
+            whitePiece.setImage(image);
+            pane.getChildren().add(whitePiece);
 
             primaryStage.setScene(scene);
             primaryStage.setTitle("Jozefina");
@@ -24,6 +44,14 @@ public class TutorialRunner extends Application {
             e.printStackTrace();
         }
     }
+
+    /*GridPane gridPane = new GridPane();
+    Scene scene = new Scene(gridPane, 400, 400);*/
+
+    Image image = new Image("file:resources/white-normal.png");
+
+    StackPane pane = new StackPane();
+    Scene scene = new Scene(pane, 500, 500);
 
     public static void main(String[] args) {
         launch(args);
